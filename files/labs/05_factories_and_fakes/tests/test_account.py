9408e1048280ebee280e291ca88f06875741bfd3
Uzair Abid <89099804+uzairabid1@users.noreply.github.com>
2023-03-03 00:52:20 +0500

    changes

--- files/labs/05_factories_and_fakes/tests/test_account.py
+++ files/labs/05_factories_and_fakes/tests/test_account.py
@@ -6,6 +6,7 @@ from random import randrange
 from unittest import TestCase
 from models import db
 from models.account import Account, DataValidationError
+from factories import AccountFactory
 
 ACCOUNT_DATA = {}
 
@@ -41,28 +42,26 @@ class TestAccountModel(TestCase):
 
     def test_create_all_accounts(self):
         """ Test creating multiple Accounts """
-        for data in ACCOUNT_DATA:
-            account = Account(**data)
+        for _ in range(10):
+            account = AccountFactory()
             account.create()
-        self.assertEqual(len(Account.all()), len(ACCOUNT_DATA))
+        self.assertEqual(len(Account.all()), 10)
 
     def test_create_an_account(self):
         """ Test Account creation using known data """
-        data = ACCOUNT_DATA[self.rand] # get a random account
-        account = Account(**data)
+        account = AccountFactory()
         account.create()
         self.assertEqual(len(Account.all()), 1)
 
     def test_repr(self):
         """Test the representation of an account"""
-        account = Account()
+        account = AccountFactory()
         account.name = "Foo"
         self.assertEqual(str(account), "<Account 'Foo'>")
 
     def test_to_dict(self):
         """ Test account to dict """
-        data = ACCOUNT_DATA[self.rand] # get a random account
-        account = Account(**data)
+        account = AccountFactory()    
         result = account.to_dict()
         self.assertEqual(account.name, result["name"])
         self.assertEqual(account.email, result["email"])
@@ -71,9 +70,9 @@ class TestAccountModel(TestCase):
         self.assertEqual(account.date_joined, result["date_joined"])
 
     def test_from_dict(self):
-        """ Test account from dict """
-        data = ACCOUNT_DATA[self.rand] # get a random account
-        account = Account()
+        """ Test account from dict """        
+        account = AccountFactory()
+        data = account.to_dict()
         account.from_dict(data)
         self.assertEqual(account.name, data["name"])
         self.assertEqual(account.email, data["email"])
@@ -82,26 +81,23 @@ class TestAccountModel(TestCase):
 
     def test_update_an_account(self):
         """ Test Account update using known data """
-        data = ACCOUNT_DATA[self.rand] # get a random account
-        account = Account(**data)
+        account = AccountFactory()
         account.create()
         self.assertIsNotNone(account.id)
         account.name = "Rumpelstiltskin"
         account.update()
         found = Account.find(account.id)
-        self.assertEqual(found.name, account.name)
+        self.assertEqual(found.name, "Rumpelstiltskin")
 
     def test_invalid_id_on_update(self):
         """ Test invalid ID update """
-        data = ACCOUNT_DATA[self.rand] # get a random account
-        account = Account(**data)
+        account = AccountFactory()
         account.id = None
         self.assertRaises(DataValidationError, account.update)
 
     def test_delete_an_account(self):
         """ Test Account delete using known data """
-        data = ACCOUNT_DATA[self.rand] # get a random account
-        account = Account(**data)
+        account = AccountFactory()
         account.create()
         self.assertEqual(len(Account.all()), 1)
         account.delete()
